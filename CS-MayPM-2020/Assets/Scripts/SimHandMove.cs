e25f0aaa0f197e8f5bbc7b608071e4dde1a61927
nakisa <nakisa.donnelly@gmail.com>
2020-06-01 21:51:33 -0600

    Session 3 (cont.)
    - Created our SimHandMove script.
    - Understanding the Start() method, Update() method and MonoBehaviour.
    - Using Input.GetKey to move a GameObject forward.

---
+++ CS-MayPM-2020/Assets/Scripts/SimHandMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimHandMove : MonoBehaviour
+{
+    public Vector3 location;
+    public Transform otherLocation;
+    public float moveSpeed;
+        
+    void Start()
+    {
+        //transform.position = location;
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+        }
+        // move back using S
+
+        // move right using D
+
+        // move left using A
+    }
+}
