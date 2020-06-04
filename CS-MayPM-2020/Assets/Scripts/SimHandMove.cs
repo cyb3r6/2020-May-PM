7ae35f6776b835ea23bc3593e3900650406f0d3b
nakisa <nakisa.donnelly@gmail.com>
2020-06-03 21:13:53 -0600

    Session 4
    - Used our Left Hand Rule for understanding transform.Rotate(). 
    - Getting mouse input from the Input Manager in our Project Settings. 
    - Using GetComponent<T> to get a Rigidbody on a gameobject.
    - Adding a jumping force on our space bar press to jump. 
    - Created a bridge rising animation.

--- CS-MayPM-2020/Assets/Scripts/SimHandMove.cs
+++ CS-MayPM-2020/Assets/Scripts/SimHandMove.cs
@@ -7,23 +7,97 @@ public class SimHandMove : MonoBehaviour
     public Vector3 location;
     public Transform otherLocation;
     public float moveSpeed;
+    public float turnSpeed;
+    public float jumpForce;
+    private Rigidbody peterRigidbody;
         
     void Start()
     {
         //transform.position = location;
+        peterRigidbody = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     
     void Update()
     {
+        #region Translational movement
+        // move forward using W
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
         }
         // move back using S
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
+        }
 
         // move right using D
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+        }
 
         // move left using A
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
+        }
+
+        // move up
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //transform.Translate(Vector3.up * Time.deltaTime * moveSpeed);
+            peterRigidbody.AddForce(Vector3.up * jumpForce);
+        }
+
+        // move down
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * moveSpeed);
+        }
+
+        // sprint?!
+
+        #endregion
+
+        #region Rotation using Keyboard
+        /*
+        // Rotation
+        // rotate left
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.down* Time.deltaTime * turnSpeed, Space.World);
+        }
+
+        // rotate right
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed, Space.World);
+        }
+
+        // rotate forward
+        if (Input.GetKey(KeyCode.Z))
+        {
+            transform.Rotate(Vector3.right * Time.deltaTime * turnSpeed, Space.World);
+        }
+
+        // rotate backward
+        if (Input.GetKey(KeyCode.C))
+        {
+            transform.Rotate(Vector3.left * Time.deltaTime * turnSpeed, Space.World);
+        }
+        */
+        #endregion
+
+        #region Rotate using Mouse
+
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * turnSpeed, Space.World);
+        transform.Rotate(Vector3.left * Input.GetAxis("Mouse Y") * turnSpeed, Space.Self);
+
+        #endregion
     }
+
+    
 }
