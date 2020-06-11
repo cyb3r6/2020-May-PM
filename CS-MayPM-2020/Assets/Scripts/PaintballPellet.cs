f794cda2a7630d3d528c139184708e4211f37ec0
nakisa <nakisa.donnelly@gmail.com>
2020-06-10 21:18:29 -0600

    Session 6
    - Using OnCollisionEntre() to detect a collision with an object of tag "Paintable". 
    - Getting the other colliders Renderer and assigning it a material. 
    - Using a static integer to keep track of the paintIndex across all instances of our class PaintballPellet. 
    - Making sure our paintIndex doesn't exceed the number of paints in our list.

---
+++ CS-MayPM-2020/Assets/Scripts/PaintballPellet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintballPellet : MonoBehaviour
+{
+    private Material paintMaterial;
+    public List<Material> paintballMaterials = new List<Material>();
+    static private int paintIndex = 0;
+    
+    void Start()
+    {
+        paintMaterial = GetComponentInChildren<Renderer>().material;
+    }
+
+    private void OnCollisionEnter(Collision theThingThatWeCollidedWith)
+    {
+        if(theThingThatWeCollidedWith.collider.tag == "Paintable")
+        {
+            theThingThatWeCollidedWith.collider.GetComponent<Renderer>().material = paintballMaterials[paintIndex];
+            paintIndex++;
+
+            if(paintIndex == paintballMaterials.Count)
+            {
+                paintIndex = 0;
+            }
+        }
+    }
+}
---
+++ CS-MayPM-2020/Assets/Scripts/ShootPaintball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootPaintball : MonoBehaviour
+{
+    public GameObject paintballPelletPrefab;
+    public Transform spawnPoint;
+    public float shootingForce;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject temp = Instantiate(paintballPelletPrefab, spawnPoint.position, spawnPoint.rotation);
+            temp.GetComponent<Rigidbody>().AddForce(temp.transform.forward * shootingForce);
+            Destroy(temp, 3);
+        }
+    }
+}
