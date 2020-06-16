3d09f28d5f9a446c90ea573f3ca85d5bd5eb9491
nakisa <nakisa.donnelly@gmail.com>
2020-06-15 21:31:00 -0600

    Session 7
    - Creating a canvas that has a background image and text.
    - Accessing the text on the canvas from another script.
    - Creating a SimHand with a hand model to non-HMD testing.
    - Enabling VR in our projects.

--- CS-MayPM-2020/Assets/Scripts/ShootPaintball.cs
+++ CS-MayPM-2020/Assets/Scripts/ShootPaintball.cs
@@ -7,6 +7,7 @@ public class ShootPaintball : MonoBehaviour
     public GameObject paintballPelletPrefab;
     public Transform spawnPoint;
     public float shootingForce;
+    public ShotCounter shotCounterScript;
     
     void Update()
     {
@@ -14,6 +15,7 @@ public class ShootPaintball : MonoBehaviour
         {
             GameObject temp = Instantiate(paintballPelletPrefab, spawnPoint.position, spawnPoint.rotation);
             temp.GetComponent<Rigidbody>().AddForce(temp.transform.forward * shootingForce);
+            shotCounterScript.shotsFired++;
             Destroy(temp, 3);
         }
     }
---
+++ CS-MayPM-2020/Assets/Scripts/ShotCounter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShotCounter : MonoBehaviour
+{
+    public Text shotCounterText;
+    public int shotsFired;
+
+    void Update()
+    {
+        shotCounterText.text = "Shots Fired: " + shotsFired;
+    }
+}
