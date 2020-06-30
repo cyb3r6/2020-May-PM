571a837bd719cdba9edb46a838c3559ec19baacb
nakisa <nakisa.donnelly@gmail.com>
2020-06-30 12:55:06 -0600

    Session 11
    - ARFoundation scene set up using ARSession and ARSessionOrigin.
    - Detecting a touch on our screen using a bool method that returns a value and outputs a Vector2. 
    - Raycasting from our phones to instantiate a Peter into the world

---
+++ CS-MayPM-2020/Assets/Scripts/AR/ARTapToPlace.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARTapToPlace : MonoBehaviour
+{
+    public GameObject objectToPlace;
+
+    private ARRaycastManager arRaycastManager;
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private Vector2 touchPosition;
+
+    void Awake()
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // if we have touched our screen and where
+    bool TryGetTouchPosition(out Vector2 touchPosition)  // = either true or false
+    {
+        if(Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+
+            return true;
+        }
+
+        touchPosition = default;    // a default value for a Vector2 (0,0)
+
+        return false;
+    }
+
+    
+    void Update()
+    {
+        // first have to detect if we've touched the screen
+        if(!TryGetTouchPosition(out Vector2 touchPosition))
+        {
+            return;
+        }
+
+        if(arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+
+            GameObject spawnedObject = Instantiate(objectToPlace, hitPose.position, hitPose.rotation);
+        }
+    }
+}
