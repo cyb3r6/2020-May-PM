b732562a5f20909a0beb18d81bdaf4522e52e572
nakisa <nakisa.donnelly@gmail.com>
2020-07-01 21:17:35 -0600

    Session 12
    - Vuforia custom image targets and tracking two images at once.
    - Creating our first shader.Understanding renderqueues in Unity (Background > Geometry/Opaque > AlphaTest > Transparent > Overlay).
    - Using GetComponent<T>() to get the renderer on a GameObject and changing its material's renderqueue through a script.

---
+++ CS-MayPM-2020/Assets/Scripts/XRayableItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XRayableItem : MonoBehaviour
+{    
+    void Awake()
+    {
+        if (GetComponent<Renderer>())
+        {
+            GetComponent<Renderer>().material.renderQueue = 3002;
+        }
+    }
+}
---
+++ CS-MayPM-2020/Assets/Shaders/XrayShader.shader
@@ -0,0 +1,8 @@
+﻿Shader "Custom/XrayShader"
+{   
+    SubShader
+    {
+        Tags { "Queue"="Transparent+1" }        // render after all transparent objects (this queue will be 3001)
+        Pass { Blend Zero One}                  // makes the object transparent
+    }
+}
